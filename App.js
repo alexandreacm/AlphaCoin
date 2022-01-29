eb1751bee0a1a742e5d3152d19d09eff42d9e8dd
Alexandre carvalho marques <alexandreacm.marques@gmail.com>
2022-01-29 13:11:58 -0300

    Configuring routes, screens and babel-config.

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import ThemeProvider from '@/components/ThemeProvider';
+
+import colors from '@/theme/colors';
+import Routes from '@/routes';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+      <StatusBar barStyle='auto' backgroundColor={colors.PRIMARY} />
+      <NavigationContainer>
+        <ThemeProvider>
+          <Routes />
+        </ThemeProvider>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({});
---
+++ src/routes/HomeStack.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from '@/screens/Login';
+import Home from '@/screens/CryptoList';
+import RealTimeChart from '@/screens/RealTimeChart';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export default function HomeStack() {
+  return (
+    <Navigator initialRouteName='Home'>
+      <Screen name='Home' component={Home} />
+      <Screen name='Login' component={Login} />
+      <Screen name='RealTimeChart' component={RealTimeChart} />
+    </Navigator>
+  );
+}
